cdf7dd8df3e83ee469e867cd26bb12e4d8dadcdf
DANGTHANHTRUNG\ACER <dangthanhtrung57@gmail.com>
2023-03-06 20:52:03 +0700

    Login page

--- coffee-contryside/src/app/modules/login/login.component.ts
+++ coffee-contryside/src/app/modules/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RoutingConstant } from 'src/app/shared/constants/routing-constants';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // Dummy data for login
+  username: string = "admin";
+  password: string = "admin";
+
+  loginForm = new FormGroup ({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  errorMessage: string = "";
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    // Get data from Form
+    const usernameValue = this.loginForm.controls["username"].value;
+    const passwordValue = this.loginForm.controls["password"].value;
+
+    // Check validation
+    if (usernameValue === this.username && passwordValue === this.password) {
+      this.router.navigate([RoutingConstant.DASHBOARD]);
+    } else {
+      this.errorMessage = "Invalid username or password.";
+    }
+  }
 }
