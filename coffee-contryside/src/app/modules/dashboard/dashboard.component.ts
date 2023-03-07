a299e2cd606d4fe77c359657b21d6cb3929fac90
DANGTHANHTRUNG\ACER <dangthanhtrung57@gmail.com>
2023-03-07 18:39:37 +0700

    Init dashboard

--- coffee-contryside/src/app/modules/dashboard/dashboard.component.ts
+++ coffee-contryside/src/app/modules/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  products: number[] = [1,2,3,4,5,6,7,8,9];
   constructor() { }
 
   ngOnInit(): void {
