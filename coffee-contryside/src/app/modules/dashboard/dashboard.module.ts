e0c7156267dcb82004e88e92b664ba3964730e91
DANGTHANHTRUNG\ACER <dangthanhtrung57@gmail.com>
2023-03-24 17:52:48 +0700

    update ui

--- coffee-contryside/src/app/modules/dashboard/dashboard.module.ts
+++ coffee-contryside/src/app/modules/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +11,8 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatButtonModule
   ]
 })
 export class DashboardModule { }
